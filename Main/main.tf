dd2082b237369c74f73391fbd3c505a21fe72fc8
Anu <143781093+anudhiman18@users.noreply.github.com>
2023-10-19 18:54:51 +0530

    Update main.tf

--- Main/main.tf
+++ Main/main.tf
@@ -10,22 +10,6 @@ module "ec2" {
 }
 
 
-# #calling modules
-# module "ec2" {
-#     source = "../ec2"
-#     for_each = var.ec2
-#     ami = each.value.ami
-#     instance_type = each.value.instance_type
-#     tag_name = each.value.tag_name
-#     owner = each.key
-# }
-
-# module "s3" {
-#     source = "../s3"
-#     bucket_name = var.bucket_name
-# }
-
-
 
 module "s3" {
     source = "../S3"
